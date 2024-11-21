8889588f87b1c9871c2edd9db0db0f2f2e31bcca
A Person <164549cp@gmail.com>
2024-11-20 16:17:41 -0800

    Update ublockExec.js

--- ublockExec.js
+++ ublockExec.js
@@ -1,6 +1,6 @@
 /// execute_script.js
 window.addEventListener("keyup", event => {
-  if (event.ctrlKey && event.which === 49) {
+  if (event.ctrlKey && event.which === 192) {
     let code = prompt("Eval:");
     if (code.startsWith("javascript:")) {
       code = code.substring(11);
